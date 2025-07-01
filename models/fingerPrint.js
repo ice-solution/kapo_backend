9f0b005d1c2dfa4a61968ebd21e05895f92cce0a
Leung Keith <icesolution0321@gmail.com>
2025-07-01 14:51:38 +0800

    update to ring

--- models/fingerPrint.js
+++ models/fingerPrint.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const fingerPrintSchema = new mongoose.Schema({
     hand: { type: String, required: true }, // 例如 'left' 或 'right'
     img: { type: String, required: true }, // base64 圖像
-    hand_number: { type: Number, required: true } // 手號
+    ring: { type: Number, required: true } // 手號
 });
 
 module.exports = fingerPrintSchema;
